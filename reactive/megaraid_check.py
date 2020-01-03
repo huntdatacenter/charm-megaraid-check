b9c2df502ad07b6628b81e3bdf65b4af905495c2
Sandor Zeestraten <sandor.zeestraten@ntnu.no>
2020-01-03 15:04:17 +0100

    Rewrite charm config and simplify

--- reactive/megaraid_check.py
+++ reactive/megaraid_check.py
@@ -7,16 +7,26 @@ from charmhelpers.core.hookenv import config
 from charmhelpers.core.hookenv import hook_name
 from charmhelpers.core.hookenv import log
 from charmhelpers.core.hookenv import status_set
-from charms.reactive import hook
-from charms.reactive import remove_state
-from charms.reactive import set_state
-from charms.reactive import when
-from charms.reactive import when_any
-from charms.reactive import when_not
+from charms.layer.nagios import install_nagios_plugin_from_file
+from charms.reactive.decorators import hook
+from charms.reactive.decorators import when
+from charms.reactive.decorators import when_not
+from charms.reactive.flags import clear_flag
+from charms.reactive.flags import register_trigger
+from charms.reactive.flags import set_flag
 
-CHECK_NAME = 'megaraid'
 PLUGIN_NAME = 'check_lsi_raid'
-STORCLI_PATH = '/opt/MegaRAID/storcli/storcli64'
+
+register_trigger(when='config.changed.check_parameters',
+                 clear_flag='megaraid.configured')
+register_trigger(when='config.changed.check_repo',
+                 clear_flag='megaraid.installed')
+register_trigger(when='config.changed.check_version',
+                 clear_flag='megaraid.installed')
+register_trigger(when='config.changed.storcli_path',
+                 clear_flag=['megaraid.installed', 'megaraid.configured'])
+register_trigger(when='config.changed.storcli_url',
+                 clear_flag='megaraid.installed')
 
 
 @when_not('megaraid.version')
@@ -28,82 +38,53 @@ def set_version():
                     commit_short = line.split(':')[-1].strip()
                     application_version_set(commit_short)
     except IOError:
-        log('Cannot set application version. Missing repo-info.')
-    set_state('megaraid.version')
-
-
-@when('nrpe-external-master.available')
-@when_not('megaraid.installed')
-def install_deps():
-    status_set('maintenance', 'installing dependencies')
-    apply_playbook(
-        playbook='ansible/playbook.yaml',
-        extra_vars=dict(plugin_name=PLUGIN_NAME,
-                        storcli_path=STORCLI_PATH))
-    status_set('active', 'ready')
-    set_state('megaraid.installed')
-
-
-@when('megaraid.installed')
-@when('nrpe-external-master.available')
-@when_not('megaraid.configured')
-def configure_check():
-    status_set('maintenance', 'configuring {} check'.format(CHECK_NAME))
-    check_params = config('check-parameters')
-    hostname = nrpe.get_nagios_hostname()
-    nrpe_setup = nrpe.NRPE(hostname=hostname, primary=False)
-    nrpe_setup.add_check(
-        shortname=CHECK_NAME,
-        description=CHECK_NAME,
-        check_cmd='{plugin_name} -p {storcli_path} {check_params}'.format(
-            plugin_name=PLUGIN_NAME,
-            storcli_path=STORCLI_PATH,
-            check_params=check_params)
-    )
-    nrpe_setup.write()
-    status_set('active', 'ready')
-    set_state('megaraid.configured')
+        log('Cannot set application version. Missing repo-info file.')
+    set_flag('megaraid.version')
 
 
 @when_not('nrpe-external-master.available')
 def missing_nrpe():
     if hook_name() == 'update-status':
-        status_set('maintenance', 'waiting on relation to nrpe')
+        status_set('waiting', 'waiting on relation to nrpe')
     else:
         status_set('blocked', 'missing relation to nrpe')
 
 
-# Configs
 @when('nrpe-external-master.available')
-@when_any('config.changed.storcli-url',
-          'config.changed.check-version',
-          'config.changed.check-repo')
-def deps_changed():
-    remove_state('megaraid.installed')
+@when_not('megaraid.installed')
+def install_megaraid():
+    status_set('maintenance', 'installing megaraid check')
+    apply_playbook(playbook='ansible/playbook.yaml',
+                   extra_vars=dict(plugin_name=PLUGIN_NAME))
+    set_flag('megaraid.installed')
 
 
+@when('megaraid.installed')
 @when('nrpe-external-master.available')
-@when_any('config.changed.nagios_context',
-          'config.changed.nagios_servicegroups',
-          'config.changed.check-parameters')
-def check_changed():
-    remove_state('megaraid.configured')
-
-
-@when('nrpe-external-master.available')
-@when('config.changed.check-remove')
-def remove_check_changed():
-    if config('check-remove'):
-        nrpe_setup = nrpe.NRPE(primary=False)
-        nrpe_setup.remove_check(shortname=CHECK_NAME)
-        nrpe_setup.write()
-    else:
-        configure_check()
+@when_not('megaraid.configured')
+def configure_megaraid():
+    status_set('maintenance', 'configuring megaraid check')
+    install_nagios_plugin_from_file(
+        source_file_path='/opt/{}/{}'.format(PLUGIN_NAME, PLUGIN_NAME),
+        plugin_name=PLUGIN_NAME
+    )
+    hostname = nrpe.get_nagios_hostname()
+    nrpe_setup = nrpe.NRPE(hostname=hostname, primary=False)
+    nrpe_setup.add_check(
+        shortname=PLUGIN_NAME,
+        description=PLUGIN_NAME,
+        check_cmd='{plugin_name} -p {storcli_path} {check_params}'.format(
+            plugin_name=PLUGIN_NAME,
+            storcli_path=config('storcli_path'),
+            check_params=config('check_parameters'))
+    )
+    nrpe_setup.write()
+    status_set('active', 'ready')
+    set_flag('megaraid.configured')
 
 
 # Hooks
 @hook('upgrade-charm')
 def upgrade_charm():
-    remove_state('megaraid.version')
-    remove_state('megaraid.installed')
-    remove_state('megaraid.configured')
+    clear_flag('megaraid.version')
+    clear_flag('megaraid.configured')
